97522dde54be15d802e86c1e7b020651d835c9f2
Darktron <omyp12@gmail.com>
2024-05-02 05:03:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget -y
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -16,32 +16,19 @@ echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -P ~/ccminer
 
-config_file=~/ccminer/config.json
-
-if [ -f "$config_file" ]; then
-    echo "A config.json file already exists in ~/ccminer."
-
-    read -p "Do you want to overwrite it? (y/n): " answer
-    case "$answer" in
-        y|Y)
-            echo "Removing existing config.json..."
-            rm "$config_file"
-            ;;
-        n|N)
-            echo "Keeping existing config.json."
-            ;;
-        *)
-            echo "Invalid input. Keeping existing config.json."
-            ;;
-    esac
+if [ -f ~/ccminer/config.json]
+then
+  mv ~/ccminer/config.json ~/ccminer/config.json.old
+  echo "A config.json file was found and renamed to config.json.old as a backup"
 fi
 
-echo "Downloading new config.json..."
+echo "Downloading latest config.json from github repo"
 wget https://raw.githubusercontent.com/low2k7/dallasccminer/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
-  mv ~/ccminer/ccminer ~/ccminer/ccminer_old
+  mv ~/ccminer/ccminer ~/ccminer/ccminer.old
+  echo "A ccminer binary was found and renamed to ccminer.old as a backup"
 fi
 mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
@@ -52,11 +39,11 @@ cat << EOF > ~/ccminer/start.sh
 EOF
 chmod +x start.sh
 
-echo "setup nearly complete."
+echo "Setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
 
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
+echo "Change address and your worker name."
+echo "Use \"<CTRL>-x\" to exit and respond with"
 echo "\"y\" on the question to save and \"enter\""
 echo "on the name"
 
