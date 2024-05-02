66c1b44a04637c016f4c0dfe88cac58dd262be5d
Darktron <omyp12@gmail.com>
2024-05-02 05:40:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo
-echo "Running update and upgrade..."
+echo "D&I updates and upgrades..."
 echo
 sudo apt update -y
 sudo apt upgrade -y
 echo
-echo "Installing dependencies..."
+echo "D&I dependencies..."
 echo
 sudo apt install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget -y
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -17,6 +17,14 @@ cd ~/ccminer
 GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
 GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].browser_download_url")
 GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].name")
+
+if [ -f ~/ccminer/ccminer ]
+then
+  mv ~/ccminer/ccminer ~/ccminer/ccminer.old
+  echo
+  echo "A ccminer binary was found and renamed to ccminer.old as a backup"
+  echo
+fi
 
 echo
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
@@ -36,14 +44,6 @@ echo "Downloading latest config.json from github repo"
 echo
 wget https://raw.githubusercontent.com/low2k7/dallasccminer/main/config.json -P ~/ccminer
 
-if [ -f ~/ccminer/ccminer ]
-then
-  mv ~/ccminer/ccminer ~/ccminer/ccminer.old
-  echo
-  echo "A ccminer binary was found and renamed to ccminer.old as a backup"
-  echo
-fi
-
 mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
 
@@ -57,10 +57,11 @@ echo
 echo "Setup nearly complete!"
 echo
 echo "Edit the config with \"nano ~/ccminer/config.json\""
-echo "Change address and worker name."
+echo "Change pools, wallet address and worker name."
 echo
 echo "Use \"CTRL + x\" to exit and respond with \"y\" on the question and to save \"enter\" on the name"
 echo
 echo "Start the miner with \"~/ccminer/start.sh\""
 echo 
 echo "Stop the miner with \"CTRL + c\""
+echo
